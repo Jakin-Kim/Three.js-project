071b1a0d624bd68521a10b4413b3706ea73d31a4
Jakin-Kim <kimyagin94@gmail.com>
2022-12-02 15:47:21 +0900

    feat: 물체(공,바닥) 및 조명 생성 완료

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import './style.scss';
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
+// 조명(light)
+function addLighting(scene) {
+  let color = 0xFFFFFF;
+  let intensity = 1;
+  let light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(0, 10, 0);
+  light.target.position.set(-5, -2, -5);
+  scene.add(light);
+  scene.add(light.target);
+}
+
+// 공(sphere)
+function addSphere(scene) {
+  let geometry = new THREE.SphereGeometry( 5, 32, 32 );
+  let material = new THREE.MeshStandardMaterial({color: 0x7fffd4, roughness: 0});
+  let sphere = new THREE.Mesh( geometry, material );
+  sphere.position.set(0, 5, 0);
+  sphere.name = 'my-sphere';
+  scene.add( sphere );
+}
+
+// 바닥(floor)
+function addFloor(scene) {
+  let geometry = new THREE.BoxGeometry(50, 1, 50);
+  let material = new THREE.MeshStandardMaterial({color: 0xDDDDDD, roughness: 0});
+  const floor = new THREE.Mesh( geometry, material );
+  floor.position.set(0, -10, 0);
+  floor.name = 'my-floor';
+  scene.add(floor);
+}
+
+// 반응형 처리
+function windowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 // scene
 const scene = new THREE.Scene();
@@ -18,3 +55,38 @@ const controls = new OrbitControls(camera, canvas);
 // renderer
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+
+addLighting(scene); // scene에 조명 추가
+addFloor(scene);    // scene에 바닥 추가
+addSphere(scene);   // scene에 공 추가
+
+// setting initial values for required parameters 
+let acceleration = 9.8;
+let bounce_distance = 18;
+let bottom_position_y = -4;
+let time_step = 0.02;
+// time_counter is calculated to be the time the ball just reached the top position
+// this is simply calculated with the s = (1/2)gt*t formula, which is the case when ball is dropped from the top position
+let time_counter = Math.sqrt(bounce_distance * 2 / acceleration);
+let initial_speed = acceleration * time_counter;
+let sphere = scene.getObjectByName("my-sphere");
+
+// 애니메이션 scene
+const animate = () => {
+  requestAnimationFrame( animate );
+
+  // reset time_counter back to the start of the bouncing sequence when sphere hits through the bottom position
+  if (sphere.position.y < bottom_position_y) {
+    time_counter = 0;
+  }
+  // calculate sphere position with the s2 = s1 + ut + (1/2)gt*t formula
+  // this formula assumes the ball to be bouncing off from the bottom position when time_counter is zero
+  sphere.position.y = bottom_position_y + initial_speed * time_counter - 0.5 * acceleration * time_counter * time_counter;
+    
+  // advance time
+  time_counter += time_step;
+  window.addEventListener('resize', windowResize);
+  renderer.render( scene, camera );
+};
+animate();
